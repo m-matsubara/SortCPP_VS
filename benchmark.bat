9aef00bca39cb8e23532466f41f1f5a9f2e4c044
matsubara <matsu.boolean@gmail.com>
2019-11-10 22:34:03 +0900

    ベンチマーク用のバッチを用意

---
+++ benchmark.bat
@@ -0,0 +1,10 @@
+@echo off
+x64\Release\QuickSort.exe
+x64\Release\mmSort.exe
+x64\Release\StdSort.exe
+
+x64\Release\MergeSort.exe
+x64\Release\MergeSortNR.exe
+x64\Release\Mas4Sort.exe
+x64\Release\MasSort.exe
+x64\Release\StdStableSort.exe
